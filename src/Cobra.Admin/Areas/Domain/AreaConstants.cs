23d82e0b8b6b5829ea3f3bd5aa6dcf86280bbd10
Renan Marcel Paulino Leite <contato@renanmarcel.me>
2021-09-14 00:59:19 -0300

    Feat: Criação das paginas de Dominio

---
+++ src/Cobra.Admin/Areas/Domain/AreaConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Cobra.Admin.Areas.Domain;
+
+/// <summary>
+/// More info: http://www.dotnettips.info/post/2550
+/// </summary>
+public static class AreaConstants
+{
+    public const string IdentityArea = "Domain";
+}
---
+++ src/Cobra.Admin/Areas/Domain/Pages/Country/Item.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cobra.Admin.Areas.Admin;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cobra.Admin.Areas.Domain.Pages.Country;
+
+[Area(AreaConstants.IdentityArea)]
+[Authorize]
+public class ItemModel : PageModel
+{
+    [BindProperty(SupportsGet = true)]
+    public Guid? Id { get; set; }
+
+    public IActionResult OnGet()
+    {
+        if (Id.HasValue)
+        {
+
+        }
+
+        return Page();
+    }
+}
